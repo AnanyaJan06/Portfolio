8d12e87ebe316299f48c4fafe7d78b02c69288b7
Ananya Jan <ananyajan2000@gmail.com>
2024-12-23 14:09:18 +0530

    Fourth Commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,27 @@
-import React from 'react'
-import Profilepic from '../assets/profilepic.jpg' 
+import Profilepic from '../assets/profilepic.jpg'
+import {motion} from "framer-motion" 
+
+const containerVariants ={
+  hidden:{opacity:0,x:-100},
+  visible:{
+    opacity:1,
+    x:0,
+    transition:{
+      duration:0.5,
+      staggerChildren:0.5,
+    }
+  }
+}
+
+const ChildVariants={
+  hidden:{opacity:0,x:-100},
+  visible:{opacity:1,
+  x:0,
+  transition:{
+    duration:0.5
+  }  
+  }
+}
 
 const Hero = () => {
   return (
@@ -7,20 +29,20 @@ const Hero = () => {
         <div className='flex flex-wrap lg:flex-row-reverse'>
             <div className='w-full lg:w-1/2'>
             <div className='flex justify-center lg:p-8'>
-                <img src={Profilepic} alt="Ananya" className='border border-stone-900 rounded-3xl' />
+                <motion.img src={Profilepic} alt="Ananya" className='border border-stone-900 rounded-3xl' height={650} width={650} initial={{x:100,opacity:0}} animate={{x:0,opacity:1}} transition={{duration:1,delay:1.5}}/>
             </div>
          </div>
          <div className='w-full lg:w-1/2'>
-           <div className='flex flex-col items-center lg:items-start mt-10'>
-            <h2 className='pb-2 text-4xl tracking-tighter lg:text-8xl'>Ananya P</h2>
-            <span className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent'>Full Stack Developer</span>
-            <p className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'>
+           < motion.div initial="hidden" animate="visible" variants={containerVariants} className='flex flex-col items-center lg:items-start mt-10'>
+            <motion.h2 variants={ChildVariants} className='pb-2 text-4xl tracking-tighter lg:text-8xl'>Ananya P</motion.h2>
+            <motion.span variants={ChildVariants} className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent'>Full Stack Developer</motion.span>
+            <motion.p variants={ChildVariants} className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'>
             A MERN stack developer at levelX with hands-on experience in building full-stack applications using MongoDB, Express.js, React, and Node.js. I have developed live projects, including a restaurant and café ordering system with QR code functionality and real-time admin notifications via WhatsApp. I specialize in creating responsive, user-friendly interfaces, integrating backend services with React, and managing databases. Additionally, I am skilled in using Tailwind CSS for efficient styling and have worked with Twilio for communication features.
-            </p>
-            <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" download className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10'>
+            </motion.p>
+            <motion.a variants={ChildVariants} href="/resume.pdf" target="_blank" rel="noopener noreferrer" download className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10'>
               Download Resume
-            </a>
-            </div> 
+            </motion.a>
+            </motion.div> 
          </div>
         </div>
     </div>
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,41 +1,92 @@
 import { FaCss3Alt, FaGitAlt, FaHtml5, FaJsSquare, FaNodeJs } from "react-icons/fa"
 import {RiReactjsLine} from "react-icons/ri"
 import {SiMongodb,SiExpress, SiTailwindcss} from "react-icons/si"
+import {motion} from 'framer-motion'
+
+const iconVariants=(duration) =>({
+    initial:{y:-10},
+    animate:{
+        y:[10,-10],
+        transition:{
+            duration:duration,
+            ease:"linear",
+            repeat:Infinity,
+            repeatType:"reverse",
+        }
+    }
+})
 
 
 const Technologies = () => {
   return (
     <div className='pb-24'>
-        <h2 className='my-20 text-center text-4xl'>Technologies</h2>
-            <div className='flex flex-wrap items-center justify-center gap-4'>
-                <div>
+        <motion.h2
+        whileInView={{opacity:1,y:0}} initial={{opacity:0,y:-100}} transition={{duration:1.5}}  className='my-20 text-center text-4xl'>Technologies
+        </motion.h2>
+            <motion.div 
+            whileInView={{opacity:1,x:0}}
+            initial={{opacity:0,x:-100}}
+            transition={{duration:1.5}}
+            className='flex flex-wrap items-center justify-center gap-4'>
+                <motion.div initial="initial" animate="animate" variants={iconVariants(2.5)}>
                      <RiReactjsLine className="text-7xl text-cyan-400" />
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div 
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(3)}
+                className="p-4">
                     <FaNodeJs className="text-7xl text-[#83CD29]"  />
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(5)} 
+                className="p-4">
                     <SiMongodb className="text-7xl text-[#47A248]"/>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div 
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(2)}
+                className="p-4">
                    <SiExpress className='text-7xl text-gray-600'/>
-                </div>
-                <div>
+                </motion.div>
+                <motion.div
+                 initial="initial"
+                 animate="animate"
+                 variants={iconVariants(6)}>
                     <FaGitAlt className="text-7xl text-[#F05033]" />
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div 
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(4)}
+                className="p-4">
                     <FaHtml5 className="text-7xl text-[#E34F26]"/>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div 
+                 initial="initial"
+                 animate="animate"
+                 variants={iconVariants(6)}
+                className="p-4">
                     <FaCss3Alt  className="text-7xl text-[#1572B6]"/>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div 
+                 initial="initial"
+                 animate="animate"
+                 variants={iconVariants(8)}
+                className="p-4">
                     <SiTailwindcss className="text-7xl text-[#06B6D4]" />
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div 
+                 initial="initial"
+                 animate="animate"
+                 variants={iconVariants(9)}
+                className="p-4">
                     <FaJsSquare className="text-7xl text-[#F7DF1E]"/>
-                </div>
-        </div>
+                </motion.div>
+        </motion.div>
     </div>
   )
 }
